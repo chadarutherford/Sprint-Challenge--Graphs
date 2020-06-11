a662e6cf0001b294e465c74c76666ed9ca088a6f
Chad Rutherford <chad_rutherford@outlook.com>
2020-06-11 17:30:06 -0400

    Added convenience method for travel

--- adv.py
+++ adv.py
@@ -1,9 +1,9 @@
+import random
+from ast import literal_eval
 from room import Room
 from player import Player
 from world import World
-
-import random
-from ast import literal_eval
+from util import Stack, Queue
 
 # Load world
 world = World()
@@ -17,7 +17,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -29,9 +29,11 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+map = {}
 
-
-# TRAVERSAL TEST - DO NOT MODIFY
+# convenience method to remove some verbosity for travel
+def travel_to(direction):
+    player.travel(direction)
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
@@ -46,17 +48,15 @@ else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
-
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
